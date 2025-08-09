51e356a8032982c7adb42193eb303a98a344d57c
José <walterandrade0001@gmail.com>
2025-08-08 21:50:43 -0300

    Detalhação do comentário do carregamento de duas variáveis no storage.py

--- src/storage.py
+++ src/storage.py
@@ -8,5 +8,5 @@ load_dotenv()
 # Load variables from the .env environment
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-DEV_ID = int(os.getenv("DEV_ID")) # only for those who developed the bot
-API_KEY_OPEN_ROUTER = os.getenv("API_KEY_OPEN_ROUTER")
+DEV_ID = int(os.getenv("DEV_ID")) # only for those who developed the bot or clone the repository
+API_KEY_OPEN_ROUTER = os.getenv("API_KEY_OPEN_ROUTER") # only for those who developed the bot or clone the repository
